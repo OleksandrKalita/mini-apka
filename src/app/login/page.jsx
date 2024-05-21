52a19abc6196de5681f1929e5d1001f3acbf37ad
Oleksandr Kalita <olexadr.kalita@gmail.com>
2024-05-21 10:40:18 +0200

    initial commit

---
+++ src/app/login/page.jsx
@@ -0,0 +1,11 @@
+import styles from "./login.module.css";
+
+export default function Login() {
+    return (
+        <main className={styles.main}>
+            <div className={styles.content_box}>
+                Hello
+            </div>
+        </main>
+    );
+}
